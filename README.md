ede6f156f7652988b4a8564a95637ccbeaf67d08
DESKTOP-BQ38RO6\Adeel <adeel.arif@fostermoore.com>
2021-03-06 11:31:49 +1300

    Chapter 2-2
    The builder pattern

--- README.md
+++ README.md
@@ -5,3 +5,7 @@ This repository contains exercises from LinkedIn learning resources on spring de
 ## Factory and Abstract Factory
 
 I am starting on one of the spring's core way that IoC leverages Factory and Abstract factory to leverage bean creation.
+
+## Builder Pattern
+
+Addresses how to build complex class objects with out having the noise of setting through different setters or constructor variations.
--- src/main/java/com/spring/learning/controller/AppController.java
+++ src/main/java/com/spring/learning/controller/AppController.java
@@ -1,9 +1,14 @@
 package com.spring.learning.controller;
 
+import com.spring.learning.factory.Contact;
+import com.spring.learning.factory.ContactBuilder;
 import com.spring.learning.factory.Pet;
 import com.spring.learning.factory.PetFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping("/")
@@ -22,5 +27,25 @@ public class AppController {
         Pet adoptedPet = petFactory.adoptPet(type, name);
         adoptedPet.feed();
         return adoptedPet;
+    }
+
+    @GetMapping("presidents")
+    public List<Contact> getPresidentContacts(){
+        List<Contact> contacts = new ArrayList<>();
+
+        // traditional way
+        Contact contact = new Contact();
+        contact.setFirstName("George");
+        contact.setLastName("Washington");
+        contact.setEmailAddress("George.Washington@myapp.com");
+        contacts.add(contact);
+
+        // Constructor way
+        contacts.add(new Contact("Abraham", "Lincon", null));
+
+        // Builder way
+        contacts.add(new ContactBuilder().setFirstName("Jhon.F").setLastName("Kennedy").setEmailAddress("jfk@application.com").build());
+
+        return contacts;
     }
 }
